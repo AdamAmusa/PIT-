ea53fa9abf4ce03cce53df5cbe0499fc9ea9ac73
Adam Amusa <Adamamusa2004@gmail.com>
2024-03-08 17:49:08 +0000

    jab uninterrupted and hit detection

--- Assets/Scripts/Player Combat.cs
+++ Assets/Scripts/Player Combat.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+
+    public Transform jabPoint;
+    public float attackRange = 0.5f;    
+    public LayerMask enemyLayers;
+
     public Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +21,42 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if(Input.GetKey("f"))
         {
-            animator.SetTrigger("Attack");
+            
+            Attack();
+            
         }
 
     }
+
+    void Attack(){
+        animator.SetTrigger("Attack"); 
+
+    }
+
+    
+    void animationFinished(){
+        Debug.Log("Animation Finished");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(jabPoint.position, attackRange, enemyLayers);
+
+        //loops through all the enemies that were hit
+        foreach(Collider2D enemy in hitEnemies){
+            Debug.Log("We hit " + enemy.name);
+        }
+
+        animator.ResetTrigger("Attack");
+    }
+
+    void OnDrawGizmosSelected(){
+        //if the jabPoint is not set, return
+        if(jabPoint == null){
+            return;
+        }
+        Gizmos.DrawWireSphere(jabPoint.position, attackRange);
+    }
+
+
+
 }
