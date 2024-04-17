cd638ef784c263c8800c6bb49f63b0518f6d4f45
Adam Amusa <Adamamusa2004@gmail.com>
2024-04-17 19:29:14 +0100

    Imported Enemy Sprite and enemy follows player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public GameObject player;
+    public Animator animator;
+    private Rigidbody2D rb;
+    private float distance;
+    public float speed;
+
+    private float hMove;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,29 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        hMove = Input.GetAxisRaw("Horizontal");
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        animator.SetFloat("Speed", Mathf.Abs(hMove));
+
+        if(hMove < 0){
+           gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+         if(hMove > 0){
+           gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         
+    }
+
+   
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(other.collider, GetComponent<Collider2D>());
+        }
     }
 }
