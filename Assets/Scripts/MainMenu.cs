04b45c1efd823f4e01c4b38fd32f1b442a3aeeee
Adam Amusa <Adamamusa2004@gmail.com>
2024-04-19 02:53:25 +0100

    Menu Button and Quit Button added in leaderboard scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+
+    public void goMainMenu()
+    {
+        SceneManager.LoadScene(2);
+    }   
+}
